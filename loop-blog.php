6905fe45d68db9c7f45a5908ef5fe418340037b1
Mo <i3ad@hotmail.de>
2013-09-14 18:54:02 +0200

    Update
    -Added Post-Format icons

--- loop-blog.php
+++ loop-blog.php
@@ -2,9 +2,42 @@
 
 				<header class="entry-header">
 					<h3 class="entry-title">
-						<?php if ( is_sticky() ) {?>
-							<span class="subheader sticky-mark"><i class="icon-pushpin"></i></span>
-						<?php } ?>
+
+						<span class="subheader format-mark">
+							<?php if ( is_sticky() ) {
+								echo '<i class="icon-pushpin"></i>';
+							
+							} elseif ( has_post_format( 'image' )) {
+	  							echo '<i class="icon-picture"></i>';
+
+	  						} elseif ( has_post_format( 'gallery' )) {
+								echo '<i class="icon-eye-open"></i>';
+
+							} elseif ( has_post_format( 'audio' )) {
+								echo '<i class="icon-music"></i>';
+
+							} elseif ( has_post_format( 'video' )) {
+								echo '<i class="icon-film"></i>';
+
+							} elseif ( has_post_format( 'link' )) {
+								echo '<i class="icon-link"></i>';
+
+							} elseif ( has_post_format( 'quote' )) {
+								echo '<i class="icon-quote-left"></i>';
+
+							} elseif ( has_post_format( 'chat' )) {
+								echo '<i class="icon-comments-alt"></i>';
+
+							} elseif ( has_post_format( 'status' )) {
+								echo '<i class="icon-info"></i>';
+
+							} elseif ( has_post_format( 'aside' )) {
+								echo '<i class="icon-asterisk"></i>';
+
+							}; ?>
+						</span>
+
+
 						<a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a>
 					</h3>
 
